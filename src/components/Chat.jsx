8de7083ef94dff8694de4e80587496a38bf50f0c
AndyDrewDev <andydrew.stamp@gmail.com>
2025-05-06 22:43:26 +0300

    feat: implement audio playback for new messages in Chat component
    - Integrated useAudio hook to play sound when a new message is received from a different user.
    - Updated effect dependencies to include playSound for proper functionality.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,29 +3,30 @@ import Message from "./Message";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useMessages } from "../hooks/useMessages";
+import useAudio from "../hooks/useAudio";
 
 const style = {
   main: `p-3 relative overflow-y-auto h-full`,
   welcomeMessage: `flex items-center justify-center text-gray-500 text-center p-4 text-base h-full `,
 };
 
-const audio = new Audio("/audio/tone.mp3");
-
 const Chat = () => {
   const scroll = useRef();
   const { messages } = useMessages();
   const [user] = useAuthState(auth);
+  // Використовуємо хук для аудіо
+  const { playSound } = useAudio("/audio/tone.mp3");
 
   useEffect(() => {
     if (scroll.current && messages?.length > 0) {
-      console.log("scroll.current", scroll.current);
       scroll.current.scrollIntoView({ behavior: "smooth" });
 
-      if (messages[messages.length - 1].uid !== auth.currentUser.uid) {
-        audio.play();
+      // Відтворення звуку при отриманні нового повідомлення від іншого користувача
+      if (messages[messages.length - 1].uid !== auth.currentUser?.uid) {
+        playSound();
       }
     }
-  }, [messages]);
+  }, [messages, playSound]);
 
   if (!user) {
     return (
